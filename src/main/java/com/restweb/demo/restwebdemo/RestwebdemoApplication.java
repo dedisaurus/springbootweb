f72b82f0e6047fe22392e1350ae5da461c15c80d
Dedi Irawan <38521675+dedisaurus@users.noreply.github.com>
2018-09-19 08:07:18 +0700

    all new

---
+++ src/main/java/com/restweb/demo/restwebdemo/RestwebdemoApplication.java
@@ -0,0 +1,12 @@
+package com.restweb.demo.restwebdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestwebdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestwebdemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/restweb/demo/restwebdemo/RestwebdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.restweb.demo.restwebdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestwebdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
